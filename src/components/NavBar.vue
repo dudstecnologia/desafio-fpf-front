f59c53cb51279f1333b64d845d765aca949e473c
Eduardo Silva <eduardo@dudstecnologia.com.br>
2021-01-23 13:58:21 -0400

    Navbar com rotas ativas

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,18 +1,16 @@
 <template>
   <b-navbar toggleable="lg" type="dark" variant="dark">
-      <div class="container">
-    <b-navbar-brand to="/">Cadastro de Projetos</b-navbar-brand>
+    <div class="container">
+      <b-navbar-brand to="/">Cadastro de Projetos</b-navbar-brand>
 
-    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-    <b-collapse id="nav-collapse" is-nav>
+      <b-collapse id="nav-collapse" is-nav>
       <b-navbar-nav class="ml-auto">
-          <b-navbar-nav>
-            <b-nav-item to="/">Projetos</b-nav-item>
-            <b-nav-item to="/cadastro">Cadastro</b-nav-item>
-        </b-navbar-nav>
+        <b-nav-item :to="{ name: 'lista' }" exact>Projetos</b-nav-item>
+        <b-nav-item :to="{ name: 'cadastro' }">Cadastro</b-nav-item>
       </b-navbar-nav>
-    </b-collapse>
+      </b-collapse>
     </div>
   </b-navbar>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -6,5 +6,7 @@ import routes from './routes'
 Vue.use(VueRouter)
 
 export default new VueRouter({
-  routes
+  routes,
+  linkActiveClass: 'active',
+  linkExactActiveClass: 'exact-active',
 })
