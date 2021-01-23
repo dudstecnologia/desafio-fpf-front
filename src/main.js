4f755c2e43ece8f4ae6e8ade850418af050f23d0
Eduardo Silva <eduardo@dudstecnologia.com.br>
2021-01-23 12:20:48 -0400

    Pacotes e configurações dos mesmos

--- src/main.js
+++ src/main.js
@@ -1,5 +1,21 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import VueRouter from 'vue-router'
+import { BootstrapVue } from 'bootstrap-vue'
+import VueSweetalert2 from 'vue-sweetalert2'
 import App from './App.vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'sweetalert2/dist/sweetalert2.min.css'
+
+Vue.use(Vuex)
+Vue.use(VueRouter)
+Vue.use(BootstrapVue)
+Vue.use(VueSweetalert2)
+Vue.use(VueAxios, axios)
 
 Vue.config.productionTip = false
 
